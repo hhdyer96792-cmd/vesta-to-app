e186db6057dba0fece955de33351c0839d9bea16
hhdyer96792-cmd <hhdyer96792@gmail.com>
2026-04-23 09:15:59 +0400

    Update app.js

--- app.js
+++ app.js
@@ -310,9 +310,6 @@ function hideSkeleton(targetId) {
 
 function initIcons() {
     if (typeof lucide !== 'undefined' && lucide.createIcons) {
-        // Удаляем все ранее созданные иконки (SVG)
-        document.querySelectorAll('svg[data-lucide]').forEach(svg => svg.remove());
-        // Создаём иконки заново
         lucide.createIcons();
     }
 }
